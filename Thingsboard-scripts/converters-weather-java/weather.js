77f8c60dfd3c81fb57205d110fd2ba9b6e589dc0
Ole Martin Steinnes <omsteinn@ntnu.stud.no>
2018-12-19 17:21:04 +0100

    Added the weather-converter used in Thingsboard.

---
+++ Thingsboard-scripts/converters-weather-java/weather.js
@@ -0,0 +1,47 @@
+// Decode an uplink message from a buffer
+// payload - array of bytes
+// metadata - key/value object
+
+/** Decoder **/
+
+// decode payload to string
+var data = decodeToJson(payload);
+var payloadStr = decodeToString(payload);
+
+var result = [];
+// decode payload to JSON
+// var data = decodeToJson(payload);
+
+var deviceType = metadata.deviceType;
+
+
+
+// Result object with device attributes/telemetry data
+result.push({
+   deviceName: data.deviceName,
+   deviceType: deviceType,
+   telemetry: {
+       temperature: data.temperature,
+       humidity: data.humidity,
+       pressure: data.pressure,
+       windSpeed: data.windSpeed,
+       rawData: payloadStr
+   }
+});
+
+/** Helper functions **/
+
+function decodeToString(payload) {
+   return String.fromCharCode.apply(String, payload);
+}
+
+function decodeToJson(payload) {
+   // covert payload to string.
+   var str = decodeToString(payload);
+
+   // parse string to JSON
+   var data = JSON.parse(str);
+   return data;
+}
+
+return result;
